b104d64adcb073d05f8d2fa12814c0981aeecaf4
dcc <liitdawn@gmail.com>
2023-11-07 09:37:22 +0800

    init

---
+++ Utilities/NewSMT_parser.py
@@ -0,0 +1,13 @@
+import z3
+
+
+# 读取z3的常量
+def getConstsFromTlaExp(tla_exp):
+    frontiers = []
+    frontiers.append(tla_exp)
+    res = []
+    while len(frontiers)>0:
+        #todo: 也许要正则匹配一下，读tla的常量，也许不用改
+        break
+    return 0
+
